5b32e3a8702692c287b7367b73e273fcebc7e525
OksanaNSukhobok <93849417+OksanaNSukhobok@users.noreply.github.com>
2023-10-06 13:32:12 +0500

    Изменение внешнего вида текстового окна

--- WinWinApp/WinWinApp/MainForm.Designer.cs
+++ WinWinApp/WinWinApp/MainForm.Designer.cs
@@ -45,6 +45,7 @@
             // 
             // txtOutput
             // 
+            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtOutput.Location = new System.Drawing.Point(35, 24);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
